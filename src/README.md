aef5d08aedbdea40b384ce8bc02ec58961bc8538
Nathália Barros <nathynanda@gmail.com>
2021-10-08 22:37:47 -0300

    desafio01 update

---
+++ src/README.md
@@ -0,0 +1,41 @@
+## To Do - gerenciador de tarefas
+
+### Requisitos
+[x] Should be able to create a new user whith `name` e `username`
+[x] Should be able to create a new todo
+[x] Should be able to list all user's todos
+[x] Should be able to update a todo(`title` e `deadline`) 
+[x] Should be able to mark a todo as done
+[x] Should be able to delete a todo
+
+
+### Regras de negócio
+[x] `name` e `username` devem ser passados dentro do corpo da requisição
+[x] Should not be able to create a new user when username already exists
+[x] A rota POST `/todos` deve receber  as propriedades  `title` e `deadline` dentro do corpo da requisição
+[x] A rota PUT `/todos/:id` deve receber as propriedades `title` e `deadline` dentro do corpo da requisição 
+[x] Should not be able to update a non existing todo
+[x] Should not be able to mark a non existing todo as done
+[x] Should not be able to delete a non existing todo
+[x] o `username` será passado pelo header da requisição. (Esse `username` recebido pelo header da requisição vem através do Middleware. Esse Middleware repassa o `user`, que no caso contem o `username`)
+
+   
+Obs: CRUD (Create, Read, Update, Delete) É um mnemônico para as quatro operações básicas de armazenamento persistente.
+
+
+### Template da aplicação
+-> Set-ExecutionPolicy -Scope CurrentUser
+-> RemoteSigned
+-> "yarn" na pasta do projeto clonado, para instalar todas as dependências
+-> "yarn dev" para startar o server
+
+### Testes automatizados
+Os testes sempre vão ficar dentro de uma pasta chamada `__tests__` dentro da pasta `src`.
+
+Dentro da pasta de testes, para cada arquivo testado na sua aplicação, existe um arquivo com o mesmo nome, com a extensão `.spec.js`.
+
+Para começar a utilizar os testes, execute o comando `yarn test` no seu terminal, e ele irá te retornar o resultado dos testes.
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,41 +1,166 @@
 const express = require('express');
-const cors = require('cors');
-
-// const { v4: uuidv4 } = require('uuid');
-
 const app = express();
-
-app.use(cors());
 app.use(express.json());
 
-// const users = [];
+//Criado pelo template
+const cors = require('cors');
+app.use(cors());
 
+//Essa parte o template colocou separado dentro do arquivo server.js
+/* Conteúdo do server.js
+const app = require('./');
+app.listen(3333);
+*/
+/* Meu conteúdo que costumo usar no index.js mesmo
+app.listen(3333);
+console.log("servidor rodando"); 
+*/
+
+////Importando o uuid, determinando uma versão específica. A versão v4 gera o id com números randômicos.
+//const {v4} = require("uuid");
+//Renomeando a função para uuidv4, para ficar mais fácil de trabalhar
+const { v4: uuidv4 } = require('uuid');
+
+const users = [];
+
+//Middleware
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
-}
+  const {username} = request.headers;
+  //const {cpf} = request.headers;
 
+  //const customer = customers.find((customer) => customer.cpf === cpf);
+  //Vou procurar dentro do meu array "customers = []" e vou comparar se o customer.cpf que eu acabei de criar aqui mesmo nessa função((customer) => customer.cpf) é igual ao cpf(cpf) passado pelo usuário no insomnia
+  //Obs: Como estamos trabalhando em memória salvando tudo num array, toda alteração feita no array(usar find por exemplo) não é retornado um novo array e sim uma referência fazendo com que qualquer alteração reflita diretamente no mesmo array original. Por isso não é preciso ficar atualizando o objeto "todo" por exemplo e devolvendo ele pra lista. Por isso também não é preciso ficar procurando o índice do todo, apagar e depois ainda atualizar ele no mesmo lugar.
+  //O método find retorna o valor(a informação)do primeiro elemento de um array que retornar true para uma função de teste fornecida.Se não houver alguma, retorna undefined.
+  const user = users.find((user) => user.username === username); 
+  //O método some retorna v ou f de uma busca e testa se ao menos um dos elementos no array passa na condição e retorna v ou f de acordo com a condição passada.    
+  
+  if(!user){
+      return response.status(404).json({error: "User not found!"});
+  };  
+
+  //Repassando informações de dentro do middleware para demais rotas(que estão chamando o middleware). 
+  request.user = user;  
+  //request.customer = customer;
+
+  //Se não existir o user, segue adiante com o next
+  return next(); 
+
+};
+
+
+//Rotas
+//Criar usuários
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const {name, username} = request.body;
+
+  const usernameAlreadyExists = users.find((user) => user.username === username);
+
+  if(usernameAlreadyExists){
+    return response.status(400).json({error: "Username already exists!"});
+  }; 
+
+  const user = {
+    id:uuidv4(),
+    name,
+    username,
+    todos: []    
+  }
+  
+  //Guardando dentro do array os dados
+  users.push(user);
+
+  return response.status(201).json(user);
 });
 
+//Listar todos de um usuário
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  //Recuperando o user de dentro do middleware
+  const {user} = request; 
+  //const {customer} = request; 
+
+  return response.json(user.todos); 
+  
 });
 
+//Criar todo
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const {title, deadline} = request.body;
+  
+  //Recuperando o user de dentro do middleware
+  const {user} = request; 
+
+  const todo = {
+    id:uuidv4(),
+    title,
+    deadline: new Date(deadline), //o deadline está vindo do usuário como string e aqui ele está fazendo um parse para uma data válida
+    created_at: new Date(),
+    done: false
+  }
+  //Usar new Date(deadline) irá realizar a transformação da string "ANO-MÊS-DIA" (por exemplo "2021-02-25") para uma data válida do JavaScript.
+  
+  user.todos.push(todo);
+
+  return response.status(201).json(todo);
+
 });
 
+//Alterar título e deadline do todo 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  //Recuperando o user de dentro do middleware
+  const {user} = request;
+
+  const {id} = request.params;
+  const {title, deadline} = request.body;
+
+  const todo = user.todos.find((todo) => todo.id === id);
+
+  if(!todo){
+    return response.status(404).json({error: "Todo not found"});
+  }
+
+  todo.title = title;
+  todo.deadline = new Date(deadline);
+
+  return response.json(todo);
 });
 
+//Update done no todo
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  //Recuperando o user de dentro do middleware
+  const {user} = request;
+
+  const {id} = request.params;
+  
+  const todo = user.todos.find((todo) => todo.id === id);  
+  
+  if(!todo){
+    return response.status(404).json({error: "This todo no exists"});  
+  }
+  
+  todo.done = true;
+  
+  return response.json(todo);  
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  //Recuperando o user de dentro do middleware
+  const {user} = request;
+
+  const {id} = request.params;
+
+  const todoIndex = user.todos.findIndex((todo) => todo.id === id);
+  //O findIndex vai pegar a posição do resultado da verificação. Se estiver na posição 0, 1, ou 2 e assim por diante, mas se não existir vai retornar -1
+
+  //Se o todoIndex for igual a -1, ele não existe
+  if(todoIndex === -1){
+    return response.status(404).json({error: "This todo no exists"});  
+  }
+
+  //splice - O splice recebe dois parâmetros: primeiro parâmetro(onde vai iniciar a remoção), segundo parâmetro(onde vai terminar a remoção, aqui nesse caso, só customer)
+  user.todos.splice(todoIndex, 1); //Então a partir da posição 2, por exemplo, exclua 1 elemento. No caso, somente o todoIndex
+
+  return response.status(204).json();
 });
 
 module.exports = app;
